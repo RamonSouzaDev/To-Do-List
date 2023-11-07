29186d0d1c893a3d1a02ff0e43f6dfc6c0a747a2
Ramon Mendes <dwmom@hotmail.com>
2023-11-07 16:14:44 -0300

    feature: adicionando relacionamentos

--- backend-api/app/Models/Task.php
+++ backend-api/app/Models/Task.php
@@ -10,4 +10,9 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'completed', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
