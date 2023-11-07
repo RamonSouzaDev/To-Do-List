99e6ef9458ab10fafd816e0868d6bf9331537390
Ramon Mendes <dwmom@hotmail.com>
2023-11-07 18:08:02 -0300

    feature: adicionando imagem do logo para projeto

--- backend-api/app/Models/Task.php
+++ backend-api/app/Models/Task.php
@@ -9,7 +9,7 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'completed', 'user_id'];
+    protected $fillable = ['title', 'completed', 'description', 'user_id'];
 
     public function user()
     {
