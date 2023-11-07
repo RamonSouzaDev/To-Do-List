39a39cd0759d6f471794b5c8b42be1a049897a22
Ramon Mendes <dwmom@hotmail.com>
2023-11-07 12:56:19 -0300

    feature: removendo bootstrap e melhorando layout

--- frontend-app/src/App.vue
+++ frontend-app/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
     <router-view @login-success="handleLoginSuccess"></router-view>
-  </div>
 </template>
 
 <script>
@@ -25,14 +23,3 @@ export default {
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend-app/src/main.js
+++ frontend-app/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/js/bootstrap.js';
 
 
 const app = createApp(App);
