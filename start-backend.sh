7aa93532b767a6567ccc21e78f69e71f49c2d38a
Ramon Mendes <dwmom@hotmail.com>
2023-11-09 16:12:25 -0300

    feature backend: adicionando comando para gerar as keys do passport

--- start-backend.sh
+++ start-backend.sh
@@ -6,5 +6,6 @@ docker exec -i backend-api_app_1 sh -c "cp .env.example .env"
 # Executar comandos
 docker exec -i backend-api_app_1 sh -c "composer install --ignore-platform-reqs && php artisan migrate && yes '' | php artisan passport:client --personal"
 
-docker exec -i backend-api_app_1 sh -c "php artisan key:generate
-"
+docker exec -i backend-api_app_1 sh -c "php artisan key:generate"
+
+docker exec -i backend-api_app_1 sh -c "php artisan passport:keys"
