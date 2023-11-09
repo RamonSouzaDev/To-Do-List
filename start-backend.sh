99cd0dc009d5ecfb2d924357254d136d3db18be9
Ramon Mendes <dwmom@hotmail.com>
2023-11-09 16:20:35 -0300

    feature backend: mudando a ordem dos comandos do ambiente

--- start-backend.sh
+++ start-backend.sh
@@ -4,10 +4,12 @@
 docker exec -i backend-api_app_1 sh -c "cp .env.example .env"
 
 # Executar comandos
-docker exec -i backend-api_app_1 sh -c "composer install --ignore-platform-reqs && yes '' | php artisan passport:client --personal"
+docker exec -i backend-api_app_1 sh -c "composer install --ignore-platform-reqs"
 
 docker exec -i backend-api_app_1 sh -c "php artisan key:generate"
 
 docker exec -i backend-api_app_1 sh -c "php artisan passport:keys"
 
 docker exec -i backend-api_app_1 sh -c "php artisan migrate"
+
+docker exec -i backend-api_app_1 sh -c "yes '' | php artisan passport:client --personal"
