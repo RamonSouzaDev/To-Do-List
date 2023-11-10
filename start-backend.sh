4d33102ac6e1531064bac477c7e6b7ac7917ebe5
Ramon Mendes <dwmom@hotmail.com>
2023-11-10 09:29:09 -0300

    feature backend:configurando os names do container para executar em windows e linux

--- start-backend.sh
+++ start-backend.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Copiar .env.example para .env no contêiner
-docker exec -i backend-api_app_1 sh -c "cp .env.example .env"
+docker exec -i backend-api_app sh -c "cp .env.example .env"
 
 # Executar comandos
-docker exec -i backend-api_app_1 sh -c "composer install --ignore-platform-reqs"
+docker exec -i backend-api_app sh -c "composer install --ignore-platform-reqs"
 
-docker exec -i backend-api_app_1 sh -c "php artisan key:generate"
+docker exec -i backend-api_app sh -c "php artisan key:generate"
 
-docker exec -i backend-api_app_1 sh -c "php artisan passport:keys"
+docker exec -i backend-api_app sh -c "php artisan passport:keys"
 
-docker exec -i backend-api_app_1 sh -c "php artisan migrate"
+docker exec -i backend-api_app sh -c "php artisan migrate"
 
-docker exec -i backend-api_app_1 sh -c "yes '' | php artisan passport:client --personal"
+docker exec -i backend-api_app sh -c "yes '' | php artisan passport:client --personal"
