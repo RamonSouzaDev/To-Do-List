f931da9d842d66d24f607652dfb03968f8d11bb7
Ramon Mendes <dwmom@hotmail.com>
2023-11-14 13:58:26 -0300

    feature backend: adicionando novos testes para aplicação

---
+++ backend-api/tests/Feature/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Testa se um usuário pode ser criado e possui relacionamento com tarefas.
+     *
+     * @return void
+     */
+    public function testUserCreationAndTaskRelation()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user, 'api');
+
+        $this->assertNotNull($user);
+        $this->assertInstanceOf(User::class, $user);
+
+        $task = Task::factory()->create();
+
+        $this->assertNotNull($task);
+        $this->assertInstanceOf(Task::class, $task);
+        $this->assertEquals($user->id, $task->user_id);
+    }
+}
