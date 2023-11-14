2e61fde8a1590e0351eeb6d3fe76392eb7dd2b0b
Ramon Mendes <dwmom@hotmail.com>
2023-11-14 13:04:00 -0300

    feature backend: adicionando cobertura de coverage

--- backend-api/tests/Feature/LoginControllerTest.php
+++ backend-api/tests/Feature/LoginControllerTest.php
@@ -2,14 +2,17 @@
 
 namespace Tests\Feature\Auth;
 
+use Mockery;
 use Tests\TestCase;
 use App\Models\User;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LoginControllerTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use WithFaker;
+    use DatabaseTransactions;
 
     /**
      * Testa o login com senhas incorretas.
@@ -31,4 +34,48 @@ class LoginControllerTest extends TestCase
         $response->assertStatus(401);
 
     }
+
+    /**
+     * Testa o login bem-sucedido.
+     *
+     * @return void
+     * @covers LoginController@login
+     */
+    public function testCanLoggin()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'Ramon Mendes',
+            'email' => 'dwmom@hotmail.com',
+            'password' => 'developer',
+        ]);
+        
+        $user = User::first();
+
+        // Faça a solicitação de login
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'developer',
+        ]);
+
+        $response->assertStatus(500);
+    }
+
+    /**
+     * Testa o login bem-sucedido.
+     *
+     * @return void
+     * @covers LoginController@logout
+     */
+    public function testLogoutLoggin()
+    {
+
+        $user = User::factory()->create();
+       
+        $this->be($user, 'api');
+
+        $response = $this->post('/api/logout');
+
+        $response->assertStatus(200);
+    }
+
 }
